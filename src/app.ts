2ffb115dada45699c835a09f4dc52be389f1db50
Giovanni mota <devgiovannimota@gmail.com>
2024-03-12 07:44:32 -0300

    add authenticate org controller

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,24 @@ import fastify from "fastify";
 import { ZodError } from "zod";
 import { orgRoutes } from "./http/controllers/orgs/org.routes";
 import { petRoutes } from "./http/controllers/pets/pet.routes";
+import fastifyJwt from "@fastify/jwt";
+import { env } from "./env";
+import fastifyCookie from "@fastify/cookie";
 
 export const app = fastify();
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "10m",
+  },
+});
+
+app.register(fastifyCookie);
 
 app.register(orgRoutes);
 app.register(petRoutes);
---
+++ src/http/controllers/orgs/authenticate.ts
@@ -0,0 +1,59 @@
+import { InvalidCredentialsError } from "@/useCases/errors/invalide-credentials-error";
+import { makeAuthenticateUseCase } from "@/useCases/factories/make-authenticate-org-use-case";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { email, password } = authenticateBodySchema.parse(request.body);
+
+  try {
+    const authenticateUseCase = makeAuthenticateUseCase();
+    const { org } = await authenticateUseCase.execute({
+      email,
+      password,
+    });
+    const token = await reply.jwtSign(
+      {
+        role: org.role,
+      },
+      {
+        sign: {
+          sub: org.id,
+        },
+      }
+    );
+
+    const refreshToken = await reply.jwtSign(
+      {
+        role: org.role,
+      },
+      {
+        sign: {
+          sub: org.id,
+          expiresIn: "7d",
+        },
+      }
+    );
+    return reply
+      .setCookie("refreshToken", refreshToken, {
+        path: "token/refresh",
+        secure: true,
+        httpOnly: true,
+        sameSite: true,
+      })
+      .status(200)
+      .send({ token });
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      reply.status(401).send({ message: error.message });
+    }
+  }
+}
